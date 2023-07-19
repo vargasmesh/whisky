a9695bcce69c364047a6d8b999e4adaadc956841
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-07-18 22:28:08 -0300

    feat: add transitions to admin dialog

--- components/AdminDialog.client.vue
+++ components/AdminDialog.client.vue
@@ -1,18 +1,31 @@
 <template>
-    <HeadlessDialog :open="store.dialogOpen" class="relative z-50">
-        <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
-        <div class="fixed inset-0 flex items-center justify-center p-4">
-            <HeadlessDialogPanel class="w-full max-w-sm rounded bg-white">
-                <HeadlessDialogTitle>Hello</HeadlessDialogTitle>
-            </HeadlessDialogPanel>
-        </div>
-    </HeadlessDialog>
+    <HTransitionRoot :show="store.dialogOpen" as="template">
+        <HDialog class="relative z-50" @close="closeDialog">
+            <HTransitionChild enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
+                leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0" as="template">
+                <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
+            </HTransitionChild>
+            <div class="fixed inset-0 flex justify-center p-4 text-center">
+                <HTransitionChild enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+                    enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+                    leave-to="opacity-0 scale-95" as="template">
+                    <HDialogPanel class="w-full max-w-sm max-h-2xl rounded bg-white">
+                        <HDialogTitle>Hello</HDialogTitle>
+                    </HDialogPanel>
+                </HTransitionChild>
+            </div>
+        </HDialog>
+    </HTransitionRoot>
 </template>
 
 <script setup lang="ts">
 import { useAdminStore } from "~/stores/admin";
 
 const store = useAdminStore()
+
+const closeDialog = () => {
+    store.dialogOpen = false
+}
 
 window.addEventListener('keydown', (e) => {
     if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,8 @@ export default defineNuxtConfig({
   css: ["@unocss/reset/tailwind-compat.css"],
   routeRules: {
     "/": { prerender: true }
+  },
+  headlessui: {
+    prefix: 'H',
   }
 })
