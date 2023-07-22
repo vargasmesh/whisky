8fa1fb45652fe838df01cfe2092991ba6a517b1b
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-07-22 12:14:59 -0300

    refactor: dialog

--- components/AdminDialog.client.vue
+++ components/AdminDialog.client.vue
@@ -1,26 +1,14 @@
 <template>
-    <HTransitionRoot :show="store.dialogOpen" as="template">
-        <HDialog class="relative z-50" @close="closeDialog">
-            <HTransitionChild enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
-                leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0" as="template">
-                <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
-            </HTransitionChild>
-            <div class="fixed inset-0 flex justify-center p-4 text-center items-center">
-                <HTransitionChild enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
-                    enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
-                    leave-to="opacity-0 scale-95" as="template">
-                    <HDialogPanel class="w-full max-w-sm rounded bg-white flex justify-center px-4 py-10">
-                        <div w-full>
-                            <a v-if="!store.isLoggedIn" :href="loginURL">Login</a>
-                            <div v-else>
-                                <WhiskyForm />
-                            </div>
-                        </div>
-                    </HDialogPanel>
-                </HTransitionChild>
+    <Dialog :show="store.dialogOpen" :close="closeDialog">
+        <HDialogPanel class="w-full max-w-sm rounded bg-white flex justify-center px-4 py-10">
+            <div w-full>
+                <a v-if="!store.isLoggedIn" :href="loginURL">Login</a>
+                <div v-else>
+                    <WhiskyForm />
+                </div>
             </div>
-        </HDialog>
-    </HTransitionRoot>
+        </HDialogPanel>
+    </Dialog>
 </template>
 
 <script setup lang="ts">
---
+++ components/Dialog.vue
@@ -0,0 +1,24 @@
+<template>
+    <HTransitionRoot :show="show" as="template">
+        <HDialog class="relative z-50" @close="close">
+            <HTransitionChild enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
+                leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0" as="template">
+                <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
+            </HTransitionChild>
+            <div class="fixed inset-0 flex justify-center p-4 text-center items-center">
+                <HTransitionChild enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+                    enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+                    leave-to="opacity-0 scale-95" as="template">
+                    <slot />
+                </HTransitionChild>
+            </div>
+        </HDialog>
+    </HTransitionRoot>
+</template>
+
+<script setup lang="ts">
+const { show } = defineProps<{
+    show: boolean,
+    close: () => void
+}>()
+</script>
